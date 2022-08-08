3efd5984eae1857acf3b75c74bfb66616b2edf6a
luis <madwinrar@gmail.com>
2022-08-07 20:40:03 -0400

    off

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
       <div className="container items-center max-w-6xl px-5 mx-auto space-y-6 text-center">
         <h1 className="text-4xl font-extrabold tracking-tight text-left text-gray-900 sm:text-5xl md:text-6xl md:text-center">
           <span className="block">
-            characters list Of <br />
+            Characters List Of <br />
             <span className="block mt-1 text-purple-500 lg:inline lg:mt-0">
-              Game of Thrones
+              Game Of Thrones
             </span>
           </span>
         </h1>
