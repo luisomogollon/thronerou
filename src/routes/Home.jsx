6f099e92cf8d67e20ec4d821288f0ed1289dba98
luis <madwinrar@gmail.com>
2022-08-08 22:34:35 -0400

    lasta

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <section className="px-2 pt-32 bg-gray-100 md:px-0">
       <div className="container items-center max-w-6xl px-5 mx-auto space-y-6 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight text-center text-gray-900 sm:text-5xl md:text-6xl md:text-center">
+        <h1 className=" font-extrabold tracking-tight text-center text-gray-900 sm:text-6xl  xl:text-6xl ">
           <span className="block">
-            Characters list of <br />
-            <span className="block mt-1  text-center text-3xl text-purple-500 lg:inline lg:mt-0">
+            Characters list  <br />
+            <span className="block mt-1  text-center text-5xl text-purple-500  sm:text-5xl md:text-6xl md:text-center lg:inline lg:mt-0">
               Game Of Thrones
             </span>
           </span>
